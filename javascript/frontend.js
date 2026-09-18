679cab6d92b45577fce7267bd9823b0da28fec4f
attilakronwald <attilakronwald@gmail.com>
2026-09-18 09:14:09 +0200

    jani

---
+++ javascript/frontend.js
@@ -0,0 +1,13 @@
+const focim = document.createElement("h1")
+focim.textContent = `DOM(Document Object Model)`
+
+const alcim = document.createElement("h4")
+alcim.textContent = `Alcim`
+
+const szoveg = document.createElement("p")
+szoveg.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est illum rem voluptatum quos officia placeat, repudiandae iure. Ipsum excepturi possimus hic incidunt dolore exercitationem dolor reiciendis totam dolorem adipisci.Voluptate dolorem ratione voluptates praesentium voluptas architecto neque, facere pariatur quisquam, consectetur eum a repellendus libero possimus ut laborum odio, consequuntur minus laudantium nisi voluptatum. Autem accusantium ducimus eligendi eum.A error in asperiores sunt excepturi, harum odit distinctio voluptatem odio ratione voluptatibus! Est rem quaerat aliquam aliquid facere nobis impedit ratione eius esse hic, adipisci nemo, quae provident corrupti!`
+
+// elemek összefuzese
+document.body.appendChild(focim)
+document.body.appendChild(alcim)
+document.body.appendChild(szoveg)
