637bdbf316517c6dc78d712fb307ca1cd36c3174
attilakronwald <attilakronwald@gmail.com>
2026-09-18 09:34:21 +0200

    szivegek

--- javascript/frontend.js
+++ javascript/frontend.js
@@ -1,13 +1,48 @@
 const focim = document.createElement("h1")
+focim.id = "focim"
 focim.textContent = `DOM(Document Object Model)`
 
 const alcim = document.createElement("h4")
+alcim.id = "alcim"
 alcim.textContent = `Alcim`
 
+const vonal = document.createElement("hr")
+vonal.id = vonal
+
 const szoveg = document.createElement("p")
+szoveg.id = "szoveg"
 szoveg.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est illum rem voluptatum quos officia placeat, repudiandae iure. Ipsum excepturi possimus hic incidunt dolore exercitationem dolor reiciendis totam dolorem adipisci.Voluptate dolorem ratione voluptates praesentium voluptas architecto neque, facere pariatur quisquam, consectetur eum a repellendus libero possimus ut laborum odio, consequuntur minus laudantium nisi voluptatum. Autem accusantium ducimus eligendi eum.A error in asperiores sunt excepturi, harum odit distinctio voluptatem odio ratione voluptatibus! Est rem quaerat aliquam aliquid facere nobis impedit ratione eius esse hic, adipisci nemo, quae provident corrupti!`
+
+const masodikszoveg = document.createElement("p")
+masodikszoveg.id = "masodikszoveg"
+masodikszoveg.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est illum rem voluptatum quos officia placeat, repudiandae iure. Ipsum excepturi possimus hic incidunt dolore exercitationem dolor reiciendis totam dolorem adipisci.Voluptate dolorem ratione voluptates praesentium voluptas architecto neque, facere pariatur quisquam, consectetur eum a repellendus libero possimus ut laborum odio, consequuntur minus laudantium nisi voluptatum. Autem accusantium ducimus eligendi eum.A error in asperiores sunt excepturi, harum odit distinctio voluptatem odio ratione voluptatibus! Est rem quaerat aliquam aliquid facere nobis impedit ratione eius esse hic, adipisci nemo, quae provident corrupti!`
+
+const harmadikszoveg = document.createElement("p")
+harmadikszoveg.id = "harmadikszoveg"
+harmadikszoveg.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est illum rem voluptatum quos officia placeat, repudiandae iure. Ipsum excepturi possimus hic incidunt dolore exercitationem dolor reiciendis totam dolorem adipisci.Voluptate dolorem ratione voluptates praesentium voluptas architecto neque, facere pariatur quisquam, consectetur eum a repellendus libero possimus ut laborum odio, consequuntur minus laudantium nisi voluptatum. Autem accusantium ducimus eligendi eum.A error in asperiores sunt excepturi, harum odit distinctio voluptatem odio ratione voluptatibus! Est rem quaerat aliquam aliquid facere nobis impedit ratione eius esse hic, adipisci nemo, quae provident corrupti!`
 
 // elemek összefuzese
 document.body.appendChild(focim)
 document.body.appendChild(alcim)
+document.body.append(vonal)
 document.body.appendChild(szoveg)
+document.body.appendChild(masodikszoveg)
+document.body.appendChild(harmadikszoveg)
+
+//formazas css-ben
+/*
+focim.style.color = "Red"
+alcim.style.color = "Blue"
+
+focim.style.textAlign = "center"
+alcim.style.textAlign = "center"
+
+
+szoveg.style.lineHeight = '1.5'
+masodikszoveg.style.lineHeight = '1.5'
+harmadikszoveg.style.lineHeight = '1.5'
+
+szoveg.style.textIndent = '1em'
+masodikszoveg.style.textIndent = '1em'
+harmadikszoveg.style.textIndent = '1em'
+*/
