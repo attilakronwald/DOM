2712893b24c897b2033d8be3fdb6b074e8600042
attilakronwald <attilakronwald@gmail.com>
2026-09-18 10:18:38 +0200

    szinek

--- javascript/frontend.js
+++ javascript/frontend.js
@@ -1,3 +1,6 @@
+const body = document.querySelector("body")
+body.id = "body"
+
 const focim = document.createElement("h1")
 focim.id = "focim"
 focim.textContent = `DOM(Document Object Model)`
@@ -21,13 +24,30 @@ const harmadikszoveg = document.createElement("p")
 harmadikszoveg.id = "harmadikszoveg"
 harmadikszoveg.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut est illum rem voluptatum quos officia placeat, repudiandae iure. Ipsum excepturi possimus hic incidunt dolore exercitationem dolor reiciendis totam dolorem adipisci.Voluptate dolorem ratione voluptates praesentium voluptas architecto neque, facere pariatur quisquam, consectetur eum a repellendus libero possimus ut laborum odio, consequuntur minus laudantium nisi voluptatum. Autem accusantium ducimus eligendi eum.A error in asperiores sunt excepturi, harum odit distinctio voluptatem odio ratione voluptatibus! Est rem quaerat aliquam aliquid facere nobis impedit ratione eius esse hic, adipisci nemo, quae provident corrupti!`
 
+const gomb = document.createElement("button")
+gomb.id = "gomb"
+gomb.textContent = "kattints"
+
+const gomb2 = document.createElement("button")
+gomb2.id = gomb2
+gomb2.textContent = "kattints"
+const gomb3 = document.createElement("button")
+gomb3.id = gomb3
+gomb3.textContent = "kattints"
+const gomb4 = document.createElement("button")
+gomb4.id = gomb4
+gomb4.textContent = "kattints"
 // elemek összefuzese
 document.body.appendChild(focim)
 document.body.appendChild(alcim)
+document.body.appendChild(gomb2)
+document.body.appendChild(gomb3)
+document.body.appendChild(gomb4)
 document.body.append(vonal)
 document.body.appendChild(szoveg)
 document.body.appendChild(masodikszoveg)
 document.body.appendChild(harmadikszoveg)
+document.body.appendChild(gomb)
 
 //formazas css-ben
 /*
---
+++ javascript/function.js
@@ -0,0 +1,17 @@
+gomb.addEventListener("click", function(){
+    alert("Segg")
+} 
+)
+
+gomb2.addEventListener("pointerover", function(){
+    body.style.backgroundColor = "Blue"
+} 
+)
+gomb3.addEventListener("pointerover", function(){
+    body.style.backgroundColor = "Red"
+} 
+)
+gomb4.addEventListener("pointerover", function(){
+    body.style.backgroundColor = "White"
+} 
+)
